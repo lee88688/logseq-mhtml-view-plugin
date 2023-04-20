15646f96ffef2d46222041c2df8ad292e275daea
lee88688 <lee88688@163.com>
2023-04-20 10:17:40 +0800

    initialize Marker when iframe load event

--- src/viewer.tsx
+++ src/viewer.tsx
@@ -46,50 +46,58 @@ export function Viewer(props: ViewerProps) {
     }
   }, [parser])
 
+  const contentWindow = iframeRef.current?.contentWindow
+
   useEffect(() => {
     if (iframeUrl && iframeRef.current) {
-      markerRef.current = new Marker({
-        rootElement: iframeRef.current,
-        eventHandler: {},
-        highlightPainter: (context, element) => {
-          element.style.textDecoration = "underline";
-          element.style.textDecorationColor = "#f6b80b";
-          if (context.serializedRange.annotation) {
-            element.style.backgroundColor = "rgba(246,184,11, 0.3)";
-          } else {
-            element.style.backgroundColor = "initial";
+      iframeRef.current.onload = () => {
+        const contentWindow = iframeRef.current.contentWindow!
+
+        markerRef.current = new Marker({
+          rootElement: contentWindow.document.body,
+          eventHandler: {},
+          highlightPainter: {
+            paintHighlight: (context, element) => {
+              element.style.textDecoration = "underline";
+              element.style.textDecorationColor = "#f6b80b";
+              if (context.serializedRange.annotation) {
+                element.style.backgroundColor = "rgba(246,184,11, 0.3)";
+              } else {
+                element.style.backgroundColor = "initial";
+              }
+            }
           }
+        })
+  
+        console.log(markerRef)
+  
+        markerRef.current.addEventListeners()
+  
+        const mousemove = (e: MouseEvent) => {
+          const { pageX, pageY } = e;
+          contentWindow.pointerPos = { x: pageX, y: pageY };
         }
-      })
-
-      console.log(markerRef)
-
-      markerRef.current.addEventListeners()
-
-      const contentWindow = iframeRef.current.contentWindow!
-
-      const mousemove = (e: MouseEvent) => {
-        const { pageX, pageY } = e;
-        contentWindow.pointerPos = { x: pageX, y: pageY };
+  
+        const mouseup = (e: MouseEvent) => {
+          const selection = contentWindow.getSelection();
+          if (!selection) return;
+          const range = selection.getRangeAt(0);
+          // this will always be null, because of web-marker's bug
+          const serialized = markerRef.current.serializeRange(range)
+          markerRef.current.paint(serialized)
+          Marker.clearSelection()
+        }
+  
+        contentWindow.addEventListener('mouseup', mouseup)
+        contentWindow.addEventListener('pointermove', mousemove)
       }
 
-      const mouseup = (e: MouseEvent) => {
-        const selection = window.getSelection();
-        if (!selection) return;
-        const range = selection.getRangeAt(0);
-        const serialized = markerRef.current.serializeRange(range)
-        markerRef.current.paint(serialized)
-        Marker.clearSelection()
-      }
-
-      contentWindow.addEventListener('mouseup', mouseup)
-      contentWindow.addEventListener('pointermove', mousemove)
-
-      return () => {
-        markerRef.current.removeEventListeners()
-        contentWindow.removeEventListener('mouseup', mouseup)
-        contentWindow.removeEventListener('pointermove', mousemove)
-      }
+      // todo
+      // return () => {
+      //   markerRef.current.removeEventListeners()
+      //   contentWindow.removeEventListener('mouseup', mouseup)
+      //   contentWindow.removeEventListener('pointermove', mousemove)
+      // }
     }
   }, [iframeUrl])
 
