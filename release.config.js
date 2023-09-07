f683dd533b4f288197f4cdcfeaa8ff57972c432c
lee <lee2py@gmail.com>
2023-09-08 00:22:49 +0800

    fix: add license

--- release.config.js
+++ release.config.js
@@ -20,7 +20,7 @@ module.exports = {
       "@semantic-release/exec",
       {
         prepareCmd:
-          "zip -qq -r logseq-plugin-mhtml-${nextRelease.version}.zip dist readme.md logo.jpg LICENSE package.json",
+          "zip -qq -r logseq-plugin-mhtml-${nextRelease.version}.zip dist readme.md logo.jpg LICENSE package.json LICENSE",
       },
     ],
     [
